7cd91e6260c2087e90dc1e24bea1d9ad30e5f82c
Arahit0gami <101989134+Arahit0gami@users.noreply.github.com>
2023-08-02 18:29:34 +0300

    Подключение к БД

---
+++ app/database.py
@@ -0,0 +1,18 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker
+from sqlalchemy.ext.asyncio import create_async_engine
+
+from .settings import SQLALCHEMY_DATABASE_URL
+
+
+engine = create_async_engine(SQLALCHEMY_DATABASE_URL, echo=False)
+Base = declarative_base()
+async_session = async_sessionmaker(
+    engine, class_=AsyncSession, expire_on_commit=False
+)
+
+
+# Dependency
+async def get_session() -> AsyncSession:
+    async with async_session() as session:
+        yield session
